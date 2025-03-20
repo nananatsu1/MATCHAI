63662fcd3269f8201dc527dde152fb2e68414871
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-03-20 15:48:13 +0900

    ✨ 高度の追加

--- customhooks/useGeolocation.ts
+++ customhooks/useGeolocation.ts
@@ -5,17 +5,19 @@ import { useState, useEffect } from "react";
 const useGeolocation = () => {
     const [latitude, setLatitude] = useState<number | null>(null);
     const [longitude, setLongitude] = useState<number | null>(null);
-    const [error, setError] = useState<string | null>(null);
+    const [altitude, setAltitude] = useState<number | null>(null);
+    const [error, setError] = useState<string | null>(null);    
 
     useEffect(() => {
         if (!("geolocation" in navigator)) {
-            setError("Geolocation is not supported by this browser.");
+            setError("位置情報が取得できないブラウザのようです");
             return;
         }
 
         const success = (position: GeolocationPosition) => {
             setLatitude(position.coords.latitude);
             setLongitude(position.coords.longitude);
+            setAltitude(position.coords.altitude);
         };
 
         const failure = (error: GeolocationPositionError) => {
@@ -25,7 +27,7 @@ const useGeolocation = () => {
         navigator.geolocation.getCurrentPosition(success, failure);
     }, []);
 
-    return { latitude, longitude, error };
+    return { latitude, longitude, altitude, error };
 };
 
 export default useGeolocation;
