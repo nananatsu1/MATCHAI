32c1769a0e50d4be1fc29997214f8ee3795627bb
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-03-24 12:40:31 +0900

    ⚡️ Animateの変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-"use client";
+'use client'
 import JoinRoomForm from "@/components/forms/JoinRoomForm";
 import AddRoomForm from "@/components/forms/AddRoomForm";
 import DataInitialize from "@/components/function/DataInitialize";
 import ComfirmLocalStorage from "@/components/function/ComfirmLocalStorage";
 import { useEffect, useState } from "react";
 import RotatingSquares from "@/components/animation/loading";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
@@ -19,49 +20,34 @@ const Home = () => {
     <div>
       <DataInitialize />
       <ComfirmLocalStorage />
-      <div
-        className="w-full h-screen flex flex-col justify-center items-center"
-        style={{ backgroundColor: "#f9f8f7" }}
-      >
-        {/* AnimatePresence を使用してフェードアウトを実装 */}
-        <AnimatePresence mode="wait">
-          {loading ? (
-            <motion.div
-              key="loading"
-              initial={{ opacity: 1 }}
-              animate={{ opacity: 0 }}
-              transition= {{ duration: 0.5 } }// フェードアウト
-            >
-              <RotatingSquares />
-            </motion.div>
-          ) : (
-            <motion.div
-              key="content"
-              initial={{ opacity: 0}} // 最初は透明
-              animate={{ opacity: 1}} // ゆっくりフェードイン
-              transition={{ duration: 0.5, ease: "easeOut" }} // ゆっくり表示
-              className="text-center"
-            >
-              <h3
-                className="text-gray-600 text-lg mb-4 font-nico"
-                style={{ color: "#7d7d7d", fontFamily: "NicoMoji" }}
-              >
-                パスワード
-              </h3>
+      <div className="w-full h-screen flex flex-col justify-center items-center" style={{ backgroundColor: "#f9f8f7" }}>
+      {loading ? (
+        <motion.div
+          initial={{ opacity: 1}} // 最初は透明
+          animate={{ opacity: [1,1,0]}} // ゆっくりフェードイン
+          transition={{ duration: 2, times: [0,0.8,1]}} // ゆっくり表示
+        >
+          <RotatingSquares />
+        </motion.div>
+      ) : (
+        <motion.div
+        initial={{ opacity: 0}} // 最初は透明＆少し下に
+        animate={{ opacity: 1 }} // ゆっくりフェードイン
+        transition={{ duration: 1.3}} // ゆっくり表示
+        className="text-center">
+          <h3 className="text-gray-600 text-lg mb-4 font-nico" style={{ color: "#7d7d7d" ,fontFamily: 'NicoMoji'}}>パスワード</h3>
 
-              <JoinRoomForm />
-              <div className="flex items-center gap-4 mb-4 w-64">
-                <div className="flex-grow border-t border-gray-300"></div>
-                <span className="text-gray-500 text-sm" style={{ fontFamily: "NicoMoji" }}>
-                  または
-                </span>
-                <div className="flex-grow border-t border-gray-300"></div>
-              </div>
-              <AddRoomForm />
-            </motion.div>
-          )}
-        </AnimatePresence>
+          <JoinRoomForm />
+          <div className="flex items-center gap-4 mb-4 w-64">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="text-gray-500 text-sm" style={{ fontFamily: 'NicoMoji' }}>または</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+          <AddRoomForm />
+        </motion.div>
+      )}
       </div>
+      
     </div>
   );
 };
