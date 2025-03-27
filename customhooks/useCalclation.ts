303e620380bbad1bb4449c8ee61a06e1e6f440b1
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-03-27 20:47:42 +0900

    リアルタイム更新を削除

--- customhooks/useCalclation.ts
+++ customhooks/useCalclation.ts
@@ -50,21 +50,13 @@ const useCalclation = () => {
 
   const debouncedUpdate = useDebouncedCallback(async () => {
     const updatedLocations = await fetchLocations();
-    const diffDistance = getDistance(
-      myLatitude ?? 0, myLongitude ?? 0,
-      updatedLocations.myLatitude ?? 0, updatedLocations.myLongitude ?? 0
-    ) ?? 0;
-    if (diffDistance > 5) {
-      setDistance(diffDistance);
-      setMyLatitude(updatedLocations.myLatitude);
-      setMyLongitude(updatedLocations.myLongitude);
-      setMyAltitude(updatedLocations.myAltitude);
-      setHostLatitude(updatedLocations.hostLatitude);
-      setHostLongitude(updatedLocations.hostLongitude);
-      setHostAltitude(updatedLocations.hostAltitude);
-    }
-  
-  }, 8000); // 8秒間隔で更新
+    setMyLatitude(updatedLocations.myLatitude);
+    setMyLongitude(updatedLocations.myLongitude);
+    setMyAltitude(updatedLocations.myAltitude);
+    setHostLatitude(updatedLocations.hostLatitude);
+    setHostLongitude(updatedLocations.hostLongitude);
+    setHostAltitude(updatedLocations.hostAltitude);
+  }, 10000); // 10秒間隔で更新
 
   useEffect(() => {
     // 初回データ取得
