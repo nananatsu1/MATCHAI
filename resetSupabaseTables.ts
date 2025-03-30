38bd6f42a1a322e61d74db2598ea64e4467bfd91
raimu10311221 <raimu10311221@yahoo.co.jp>
2025-03-31 04:46:51 +0900

    .tsファイルの変更

--- resetSupabaseTables.ts
+++ resetSupabaseTables.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function resetTables() {
